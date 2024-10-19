2fc7b990afa01534a35a018c1ed931a81e339193
rendob <55734639+rendob@users.noreply.github.com>
2024-10-19 13:16:51 +0900

    parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+/// compress videos in a directory
+#[derive(Parser)]
+struct Args {
+    /// path of the directory that contains uncompressed videos
+    input_dir: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args.input_dir);
 }
