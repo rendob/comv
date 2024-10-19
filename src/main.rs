c7085afa79e326aa018dbe873b686ede4188f43a
rendob <55734639+rendob@users.noreply.github.com>
2024-10-19 15:54:26 +0900

    get files recursively

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::{
+    fs::{self},
+    io::Error,
+    path::{Path, PathBuf},
+};
+
 use clap::Parser;
 
 /// compress videos in a directory
@@ -7,7 +13,30 @@ struct Args {
     input_dir: String,
 }
 
+fn get_files_recursively<P: AsRef<Path>>(dir_path: P) -> Result<Vec<PathBuf>, Error> {
+    let mut paths: Vec<PathBuf> = Vec::new();
+
+    let entries = fs::read_dir(dir_path)?;
+    for entry in entries {
+        let entry = entry?;
+        let file_type = entry.file_type()?;
+        if file_type.is_file() {
+            paths.push(entry.path());
+        } else if file_type.is_dir() {
+            let mut children = get_files_recursively(entry.path())?;
+            paths.append(&mut children);
+        }
+    }
+
+    Ok(paths)
+}
+
 fn main() {
     let args = Args::parse();
-    println!("{:?}", args.input_dir);
+
+    let output_dir = format!("{}_compressed", &args.input_dir);
+    println!("{}", output_dir);
+
+    let file_paths = get_files_recursively(args.input_dir);
+    println!("{:#?}", file_paths);
 }
